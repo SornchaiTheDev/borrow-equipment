e2886abd499eb99cf52ded922874f3abf9e3e5e9
Sornchai Somsakul <54209864+SornchaiTheDev@users.noreply.github.com>
2022-08-18 19:11:27 +0700

    Remove : Borrow Page

--- src/pages/Borrow.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function Borrow() {
-  return (
-    <div className="py-4 px-16 h-screen flex justify-center items-center w-full"></div>
-  );
-}
-
-export default Borrow;
