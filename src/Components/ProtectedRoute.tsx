53b79ba104bcb4e7ba15bfaf0bdb43ad520c1020
Sornchai Somsakul <54209864+SornchaiTheDev@users.noreply.github.com>
2022-08-23 17:14:48 +0700

    Update : ProtectRoute

--- src/Components/ProtectedRoute.tsx
+++ src/Components/ProtectedRoute.tsx
@@ -1,10 +1,17 @@
+import { useEffect } from "react";
 import { useLocalStorage } from "usehooks-ts";
 import { Navigate } from "react-router-dom";
 import { ReactNode } from "react";
+import { getDocument } from "../firebase/method/db";
 
 function ProtectedRoute({ children }: { children: JSX.Element }) {
-  const [user] = useLocalStorage("uid", "");
-  if (!user) return <Navigate to="/login" replace />;
+  const [uid, setUid] = useLocalStorage("uid", "");
+  useEffect(() => {
+    getDocument(`users/${uid}`).then(
+      (result) => result === undefined && setUid("")
+    );
+  }, []);
+  if (!uid) return <Navigate to="/login" replace />;
   return children;
 }
 
